dea112490054a5587120f1b6c67edb18aae83530
Shagun Pundir <thakurshagun.077@gmail.com>
2025-09-28 08:34:35 +0530

    commit 1

---
+++ src/class1.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class class1 {
+    public static void main (String[]args){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.print("*\n**\n***\n****\n*****\n");
+        System.out.print("******\n    *\n   *\n  *\n ******");
+
+    }
+
+
+}
