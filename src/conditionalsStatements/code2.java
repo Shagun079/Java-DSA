183d4218aedc8c0bea1e5e0d2d781736cc450aac
Shagun Pundir <thakurshagun.077@gmail.com>
2025-09-30 23:05:48 +0530

    commit 1

--- src/conditionalsStatements/code2.java
+++ src/conditionalsStatements/code2.java
@@ -1,4 +1,27 @@
 package conditionalsStatements;
 
+import java.util.*;
+
 public class code2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt ();
+
+        for(int i=0;i<t;i++) {
+            int n = sc.nextInt ();
+
+            int count = 0;
+            for (int div = 2; div * div <= n; div++) {
+                if (n % div == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println ("prime");
+            }else{
+                System.out.println ("not prime");
+            }
+        }
+    }
 }
